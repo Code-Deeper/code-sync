29d09ca6a18e8f0e25f50d39bc36a4be6e7f0e47
codewithvk <vivekpatel7202@gmail.com>
2021-10-18 22:48:50 +0530

    Tailwind Css Added

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux'
 import store from './store'
 import axios from 'axios'
 import './index.css'
-import './bootstrap.min.css';
+// import './bootstrap.min.css';
 axios.interceptors.request.use((req) => {
   if (localStorage.getItem('authUser')) {
     req.headers.Authorization = 'Bearer ' + JSON.parse(localStorage.getItem('authUser')).token;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,31 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        colors: {
+            gray: colors.coolGray,
+            blue: colors.lightBlue,
+            red: colors.rose,
+            pink: colors.fuchsia,
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        }
     },
     variants: {
-        extend: {},
-    },
-    plugins: [],
+        extend: {
+            borderColor: ['focus-visible'],
+            opacity: ['disabled'],
+        }
+    }
 }
