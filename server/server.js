5b56ce2b5c91b06b8778431d0c11f7e56d1087f7
codewithvk <vivekpatel7202@gmail.com>
2021-11-03 11:56:32 +0530

    Produc test

--- server/server.js
+++ server/server.js
@@ -80,15 +80,15 @@ io.on('connection', (socket) => {
 });
 
 // Production Settings
-if (process.env.NODE_ENV === 'production') {
-  // Set Value
-  app.use(express.static('client/build'))
+// if (process.env.NODE_ENV === 'production') {
+//   // Set Value
+//   app.use(express.static(path.join(__dirname, '/client/build')))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'..', 'client', 'build', 'index.html'));
-  })
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname,'..', 'client', 'build', 'index.html'));
+//   })
 
-}
+// }
 server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
