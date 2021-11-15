449e8f40da3b213d5f71d7a18e41fdca12f2234c
codewithvk <vivekpatel7202@gmail.com>
2021-11-15 10:06:44 +0530

    asa

--- server/server.js
+++ server/server.js
@@ -31,11 +31,7 @@ app.use(
 );
 // Controllers
 
-app.all('/*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+app.options('*', cors());
 app.get("/", (req, res) => {
   res.send("API IS RUNNING")
 })
