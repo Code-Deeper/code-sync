afa535508817294c9c7c79619de73fb2b74fe90a
codewithvk <vivekpatel7202@gmail.com>
2021-11-25 10:15:57 +0530

    SEO Friendly with favicon

--- client/src/App.js
+++ client/src/App.js
@@ -16,10 +16,11 @@ import Trial from './trial';
 import ReactGA from 'react-ga';
 import TeamPage from './Components/Team Page/TeamPage'
 import NotFound from './Components/NotFound/NotFound';
+import { Helmet } from "react-helmet";
 const cookies = new Cookies();
 const history = createBrowserHistory();
 
-const isAuthenticated =  JSON.parse(localStorage.getItem('authUser'))?.token
+const isAuthenticated = JSON.parse(localStorage.getItem('authUser'))?.token
 function App() {
   // useEffect(() => {
   //   console.log({ authroot: isAuthenticated });
@@ -27,7 +28,7 @@ function App() {
   useEffect(() => {
     ReactGA.initialize('UA-213485416-1');
     ReactGA.pageview(window.location.pathname + window.location.search);
-  },[])
+  }, [])
 
   const [loginUser, setLoginUser] = useState(null)
 
@@ -36,20 +37,25 @@ function App() {
 
 
   return (
-    
-    <Router history={history}>
-      <Route path="/" component={FrontPage} exact />
-      <Route path="/login" render={(props) => <LoginPage {...props} />} exact />
-      <Route path="/register" component={Register} exact />
-      <ProtectedRoute path='/room' component={HomePage} exact />
-      <Route path='/room/:id' render={(props) => (isAuthenticated ? <Room {...props} /> : <Redirect to="/login" />)} exact />
-      <ProtectedRoute path='/joinRoom' render={JoinRoom} exact />
-      <Route path='/newRoom' render={(props) => isAuthenticated ? <CreateRoom {...props} /> : <Redirect to="/login" />} exact />
-      <Route path='/editor' render={Editor} exact />
-      <Route path='/trial' render={(props) => <Trial {...props} />} exact />
-      <Route path="/team" component={TeamPage} exact />
+    <>
+      <Helmet >
+        <title>CodeSync</title>
+        <meta name="description" content="Cloud collaborative tool for student , Teachers and Professional with real time code editor or IDE, Realtime drawing board , Real time chat app "/>
+        </Helmet>
+      <Router history={history}>
+        <Route path="/" component={FrontPage} exact />
+        <Route path="/login" render={(props) => <LoginPage {...props} />} exact />
+        <Route path="/register" component={Register} exact />
+        <ProtectedRoute path='/room' component={HomePage} exact />
+        <Route path='/room/:id' render={(props) => (isAuthenticated ? <Room {...props} /> : <Redirect to="/login" />)} exact />
+        <ProtectedRoute path='/joinRoom' render={JoinRoom} exact />
+        <Route path='/newRoom' render={(props) => isAuthenticated ? <CreateRoom {...props} /> : <Redirect to="/login" />} exact />
+        <Route path='/editor' render={Editor} exact />
+        <Route path='/trial' render={(props) => <Trial {...props} />} exact />
+        <Route path="/team" component={TeamPage} exact />
 
-    </Router>
+      </Router>
+    </>
   );
 }
 
